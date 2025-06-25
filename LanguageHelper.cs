418e15332921842659d633d890d59a0903eb2b89
CheezyPuff38 <cheezypuff38@gmail.com>
2025-06-24 21:59:46 -0500

    finished all teh days import

--- LanguageHelper.cs
+++ LanguageHelper.cs
@@ -1,18 +1,23 @@
-﻿
-using Microsoft.JSInterop;
+﻿using Microsoft.JSInterop;
+using System.Threading.Tasks;
 
 public class LanguageHelper
 {
     private readonly IJSRuntime _jsRuntime;
 
+    // The constructor gets the JS runtime via dependency injection
     public LanguageHelper(IJSRuntime jsRuntime)
     {
         _jsRuntime = jsRuntime;
     }
 
-    public async Task<bool> GetLanguageState()
+    // This method will be called from each page to check the setting
+    public async Task<bool> GetIsTraditional()
     {
-        var langPref = await _jsRuntime.InvokeAsync<string>("localStorage.getItem", "languagePreference");
-        return langPref == "traditional"; // Returns true for Traditional, false for Modern
+        // We use "language" to match the key set by your LanguageToggle component
+        var langPref = await _jsRuntime.InvokeAsync<string>("localStorage.getItem", "language");
+
+        // This will return 'true' if the stored value is "traditional", and 'false' otherwise
+        return langPref == "traditional";
     }
 }
